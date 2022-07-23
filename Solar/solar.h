28a4e0b2daff4b9ce1e36288f20b7006c4421357
Ryan Charlton <pmyrc10@nottingham.ac.uk>
2022-07-23 15:33:52 +0100

    solar.h

---
+++ Solar/solar.h
@@ -0,0 +1,21 @@
+#ifndef SOLARRYAN
+#define SOLARRYAN
+
+#define BUFFERSIZE 32
+#define SSPORT 1234
+#define MAXCLIENTS 8
+#define SERVERPLANETS 1
+#define NAMEMAX 32
+#define SEC 1000
+#define MIN 60 * SEC
+
+#define TRUE 1
+#define FALSE 0
+
+#define LOCALHOST "127.0.0.1"
+
+#define TESTMSG "this is a test\n"
+
+//one unit in coord space represents 1 megameter
+
+#endif
---
+++ Solar/todo.md
@@ -0,0 +1,4 @@
+create planet from commandline args
+player sends positon
+servesr returns with other planet pos
+
